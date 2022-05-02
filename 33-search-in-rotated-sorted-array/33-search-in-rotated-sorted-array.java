967554c29f84507493bf0c9c20ca2cfd2e4ca982
charlenerocha <63060884+charlenerocha@users.noreply.github.com>
2022-05-02 17:56:23 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (98.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,15 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // boolean rev=false;
-        // if(nums[0]<nums.length){
-        //     rev=true;
-        // }
-        
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                return i;
+        if(nums.length-nums[0]<nums.length-nums[0]+target){
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]==target){
+                    return i;
+                }
+            }
+        }else{
+            for(int i=nums.length-1;i>=0;i--){
+                if(nums[i]==target){
+                    return i;
+                }
             }
         }
+        
+        
         
         return -1;
     }
