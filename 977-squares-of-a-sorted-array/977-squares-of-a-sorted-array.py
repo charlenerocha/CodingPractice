38a87d462071a7559e733bccb433b12649064536
charlenerocha <63060884+charlenerocha@users.noreply.github.com>
2022-05-25 13:35:58 -0400

    Time: 343 ms (16.37%), Space: 15.5 MB (73.20%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        for i, num in enumerate(nums):
-            nums[i] = num * num
         
-        return sorted(nums)
+        ans = []
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            if abs(nums[left]) < abs(nums[right]):
+                ans.insert(0, nums[right] * nums[right])
+                right = right - 1
+            else:
+                ans.insert(0, nums[left] * nums[left])
+                left = left + 1
+        
+        return ans
