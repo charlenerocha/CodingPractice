0fef4befae56e84411553c4606636280040973b0
Charlene Rocha <63060884+charlenerocha@users.noreply.github.com>
2022-06-28 11:29:45 -0400

    Time: 61 ms (76.64%), Space: 14.5 MB (22.20%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i, num1 in enumerate(nums):
-            for j, num2 in enumerate(nums):
-                if num1 + num2 == target and i is not j:
-                    return [i, j]
-                
-        return -1
+        trackedNums = {}
+        
+        for i, num in enumerate(nums):
+            if target - num in trackedNums:
+                return [i, trackedNums[target - num]]
+            trackedNums[num] = i
+            
