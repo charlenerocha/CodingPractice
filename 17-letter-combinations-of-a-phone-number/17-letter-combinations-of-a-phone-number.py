462911c3a8c9e5ae275c47b201302af3f6965c87
Charlene Rocha <63060884+charlenerocha@users.noreply.github.com>
2022-07-06 14:43:05 -0400

    Time: 27 ms (45.86%), Space: 13.4 MB (66.62%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -8,30 +8,18 @@ class Solution(object):
         if digits == "":
             return []
         
+        numToLetter = {'2': ['a', 'b', 'c'], '3': ['d', 'e', 'f'], '4': ['g', 'h', 'i'], '5': ['j', 'k', 'l'], '6': ['m', 'n', 'o'], '7': ['p', 'q', 'r', 's'], '8': ['t', 'u', 'v'], '9': ['w', 'x', 'y', 'z']}
         
-        def backTrack(path, index):
+        def backTrack(index, path):
             if len(path) == len(digits):
-                combos.append("".join(path))
+                allCombos.append(path)
                 return
+                
+            myLetters = numToLetter[digits[index]]
             
-            myLet = myHash[digits[index]]
+            for letter in myLetters:
+                backTrack(index + 1, path + letter)
             
-            for letter in myLet:
-                path.append(letter)
-                backTrack(path, index + 1)
-                path.pop()
-            
-        
-        myHash = {}
-        myHash['2'] = ['a', 'b', 'c']
-        myHash['3'] = ['d', 'e', 'f']
-        myHash['4'] = ['g', 'h', 'i']
-        myHash['5'] = ['j', 'k', 'l']
-        myHash['6'] = ['m', 'n', 'o']
-        myHash['7'] = ['p', 'q', 'r', 's']
-        myHash['8'] = ['t', 'u', 'v']
-        myHash['9'] = ['w', 'x', 'y', 'z']
-        
-        combos = []
-        backTrack([], 0)
-        return combos
+        allCombos = []
+        backTrack(0, "")
+        return allCombos
