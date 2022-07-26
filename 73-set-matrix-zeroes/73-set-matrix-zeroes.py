d2dc8d8dc95bebc87b7584659e1903cf7ba7be79
Charlene Rocha <63060884+charlenerocha@users.noreply.github.com>
2022-07-26 11:06:30 -0400

    Time: 207 ms (14.89%), Space: 14.3 MB (35.84%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -13,21 +13,19 @@ class Solution(object):
         
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                if matrix[i][j] == 0:
-                    if i not in iCovered:
-                        iCovered[i] = i
-                    if j not in jCovered:
-                        jCovered[j] = j
-                if i in iCovered:
-                    matrix[i][j] = 0
-                if j in jCovered:
-                    matrix[i][j] = 0
-        
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if i in iCovered:
-                    matrix[i][j] = 0
-                elif j in jCovered:
-                    matrix[i][j] = 0
+                if matrix[i][j] == 0 and (i not in iCovered or j not in jCovered):
                     
-                
+                    for prevI in range(0, i):
+                        matrix[prevI][j] = 0
+                    
+                    for prevJ in range(0, j):
+                        matrix[i][prevJ] = 0
+                        
+                    iCovered[i] = i
+                    jCovered[j] = j
+                else:  
+                    if i in iCovered:
+                        matrix[i][j] = 0
+                    if j in jCovered:
+                        matrix[i][j] = 0
+        
