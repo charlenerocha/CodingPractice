1bfbd84ff99d3c93087f155c6f27f67f27bdf173
Charlene Rocha <63060884+charlenerocha@users.noreply.github.com>
2023-02-04 20:19:19 -0500

    Time: 69 ms (79.23%), Space: 13.5 MB (95.19%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -6,44 +6,36 @@ class Solution(object):
         :rtype: float
         """
         
-        # using the binary search approach, rather than merging the arrays, I will try to partition them
-        # by adjusting the partitions, I'll get a better estimate of the "middle value"
+        # SECOND ATTEMPT
+        
+        # unoptimized: combine both lists in a sorted order, traverse to find the median
+        # optimized: find the combined left side, then you have your median!!
 
+        # initial guess
         totalLength = len(nums1) + len(nums2)
-        mid = int(totalLength / 2)
+        medianSpot = totalLength / 2
 
-        # [1, 2, 3, 4, 5]
-        # [3, 4, 5, 6]
-        # totalLength = 9 & mid = 4
+        median1 = (len(nums1) / 2) - 1
+        median2 = (medianSpot - median1) - 2
 
-        i = int(len(nums1) / 2)     # tracks nums1 left partition
-        j = mid - i                 # tracks nums2 left partition
-
+        # until left interval is found correctly
         while True:
-            # find values of partitions
-            if i < len(nums1):  r1 = nums1[i]
-            else:               r1 = float("infinity")
-            
-            if j < len(nums2):  r2 = nums2[j]
-            else:               r2 = float("infinity")
-            
-            if i - 1 >= 0:      l1 = nums1[i - 1] 
-            else:               l1 = float("-infinity")
-            
-            if j - 1 >= 0:      l2 = nums2[j - 1] 
-            else:               l2 = float("-infinity")
+            l1 = nums1[median1] if (median1 >= 0 and median1 < len(nums1)) else float("-infinity")
+            l2 = nums2[median2] if (median2 >= 0 and median2 < len(nums2)) else float("-infinity")
+                
+            r1 = nums1[median1 + 1] if (median1 + 1 >= 0 and median1 + 1 < len(nums1)) else float("infinity")
+            r2 = nums2[median2 + 1] if (median2 + 1 >= 0 and median2 + 1 < len(nums2)) else float("infinity")
 
-            # if the partitions are created accurately
             if l1 <= r2 and l2 <= r1:
+                # if there are two in the middle
                 if totalLength % 2 == 0:
                     return (max(l1, l2) + min(r1, r2)) / 2.0
                 else:
                     return min(r1, r2)
+            # adjust interval
             elif l1 > r2:
-                i -= 1
-                j += 1
+                median1 -= 1
+                median2 += 1
             else:
-                i += 1
-                j -= 1
-
-
+                median1 += 1
+                median2 -= 1
