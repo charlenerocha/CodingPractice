6d1879dfed330b5d0f96487732cf2e1a2cf1c5d5
Charlene Rocha <63060884+charlenerocha@users.noreply.github.com>
2023-05-29 21:05:00 -0400

    Time: 41 ms (50.38%), Space: 16.4 MB (6.92%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,20 +1,22 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        # "   fly me   to   the moon  "
         
-        # where the last letter of the last word is
-        lastWordIndex = len(s) - 1
+        isCharSeen = False
+        lastWordCount = 0
+        i = len(s) - 1
         
-        while s[lastWordIndex] == " ":
-            if lastWordIndex < 0:
-                return 0
-            lastWordIndex -= 1
-            
-        lastWordCounter = 0
-        i = lastWordIndex
-        while s[i] != " " and i >= 0:
-            lastWordCounter += 1
-            i -= 1
+        while i >= 0:
+            if isCharSeen == True:
+                if s[i] == " ":
+                    return lastWordCount
+                else:
+                    lastWordCount += 1
+            else:
+                if s[i] != " ":
+                    isCharSeen = True
+                    lastWordCount += 1
+                
+            i-=1
         
-        return lastWordCounter
+        return lastWordCount
     
