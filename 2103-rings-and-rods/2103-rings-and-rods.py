465e0e0a6487b84ee8202281059030ba4cbe1bc0
Charlene Rocha <63060884+charlenerocha@users.noreply.github.com>
2023-09-08 19:13:27 -0400

    Time: 33 ms (91.89%), Space: 16.4 MB (29.26%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -3,19 +3,24 @@ class Solution:
         # [hasRed, hasGreen, hasBlue]
         
         trackRings = {}
+        notPossible = set()
         
         for i in range(1, len(rings), 2):
             col, ind = rings[i-1], rings[i]
             
-            if ind not in trackRings:
-                trackRings[ind] = set()
-                
-            if col == 'R':
-                trackRings[ind].add('R')
-            elif col == 'G':
-                trackRings[ind].add('G')
-            elif col == 'B':
-                trackRings[ind].add('B')
+            if ind not in notPossible:
+                if rings.count(ind) < 3:
+                    notPossible.add(ind)
+                    
+                if ind not in trackRings:
+                    trackRings[ind] = set()
+
+                if col == 'R':
+                    trackRings[ind].add('R')
+                elif col == 'G':
+                    trackRings[ind].add('G')
+                elif col == 'B':
+                    trackRings[ind].add('B')
                 
         return sum(1 for val in trackRings.values() if len(val) == 3)
         
