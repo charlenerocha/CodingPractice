669c02ae1ea1c5d230e6a2392e2a43fd564e9fc2
Charlene Rocha <63060884+charlenerocha@users.noreply.github.com>
2023-09-10 19:21:39 -0400

    Time: 40 ms (84.11%), Space: 16.3 MB (78.06%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -1,10 +1,12 @@
 class Solution:
     def greatestLetter(self, s: str) -> str:
         seen = set(k for k in s)
+        checked = set()
         greatest = ""
         
         for letter in seen:
-            if letter.upper() in seen and letter.lower() in seen and letter > greatest:
+            if letter not in checked and letter.upper() in seen and letter.lower() in seen and letter > greatest:
                 greatest = letter
+            checked.add(letter)
             
         return greatest.upper()
